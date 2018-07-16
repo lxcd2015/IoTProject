07ed8d9e5812159377b398aabc65464a66343cf1
lxcd2015 <1747792658@qq.com>
2018-07-16 23:33:57 +0800

    初始化

--- startup/database.go
+++ startup/database.go
@@ -3,12 +3,14 @@ package startup
 import (
 	. "iotproject/models/datamodels"
 
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/IoTProject")
+
+	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("ConnectionStrings"))
 
 	// register model
 	orm.RegisterModel(new(TestModel))
